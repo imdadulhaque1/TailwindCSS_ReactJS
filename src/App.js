62d21e8667955ec7988abe9ede52940f9ff6eae5
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-11-11 19:19:48 +0600

    Awesome Top Navbar is designed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MainTopNavbar from './Components/Navigation/TopNavbar/MainTopNavbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainTopNavbar />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Navigation/TopNavbar/MainTopNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopNavbar from './TopNavbar'
+
+const MainTopNavbar = () => {
+    return (
+        <div className='h-screen flex justify-center items-center bg-gray-700'>
+            <TopNavbar />
+        </div>
+    )
+}
+
+export default MainTopNavbar;
